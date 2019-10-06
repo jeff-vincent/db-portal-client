06a086b96851e5e92a20faf2de8acfe72d4f7cdd
Jeff Vincent <jeff.d.vincent@gmail.com>
2019-10-06 13:27:40 -0700

    dynamic button

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 
 REQUEST_URI = 'https://sleepy-scrubland-22421.herokuapp.com/property/'
 NEXT_REQUEST_URI = ''
+PREVIOUS_REQUEST_URI = ''
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -18,15 +19,18 @@ def query():
     r = requests.get(REQUEST_URI)
     data = r.json()
     NEXT_REQUEST_URI = data['next']
+
     return data
 
 
 @app.route('/next-query', methods=['GET'])
 def nextquery():
     global NEXT_REQUEST_URI
+    global PREVIOUS_REQUEST_URI
     r = requests.get(NEXT_REQUEST_URI)
     data = r.json()
     NEXT_REQUEST_URI = data['next']
+    PREVIOUS_REQUEST_URI = data['previous']
     return data
 
 
